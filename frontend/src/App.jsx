ae294b1e63f57861cff4c33f4b440cdf9520a26c
Parship Chowdhury <parshipchowdhury@gmail.com>
2026-07-20 23:49:28 +0530

    feat: add routing and unique document IDs with react-router-dom and uuid
    Signed-off-by: Parship Chowdhury <parshipchowdhury@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import Box from '@mui/material/Box'
 import Editor from './component/Editor'
 import Header from './component/Header'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
 
 function App() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '97vh' }}>
-      <Header />
-      <Editor />
-    </Box>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/doc/${uuid()}`} />} />
+        <Route path={"/doc/:id"} element={<Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '97vh' }}>
+          <Header />
+          <Editor />
+        </Box>} />
+      </Routes>
+    </Router>
   )
 }
 
