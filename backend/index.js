0c1f90dbd2205d6157a45d0341ff6f8ab2f2f15c
Parship Chowdhury <parshipchowdhury@gmail.com>
2026-07-20 15:44:16 +0530

    feat: implement data sharing using socket.io
    Signed-off-by: Parship Chowdhury <parshipchowdhury@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import { Server } from 'socket.io'
 
 const io = new Server(3000, {
+  // Only requests from http://localhost:5173 using GET and POST are allowed.
+  // Without this configuration, the browser would block the Socket.IO connection
+  // because the frontend and backend are on different origins.
   cors: {
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
 })
 
+// Registers an event listener for the "connection" event
 io.on('connection', (socket) => {
-  console.log('a user connected', socket.id)
+  socket.on('text-change', (delta) => {
+    socket.broadcast.emit('receive-text-change', delta)
+  })
 })
